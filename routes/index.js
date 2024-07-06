17b4d78c134ab223e3470718fb2633d29518839e
Dundo <107737568+ganud@users.noreply.github.com>
2024-07-06 12:03:34 -0400

    Switch to pug, finish templates and logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleString("en-US", { timeZoneName: "short" }),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleString("en-US", { timeZoneName: "short" }),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Express" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date().toLocaleString("en-US", { timeZoneName: "short" }),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
